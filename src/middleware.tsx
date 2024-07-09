9c894b6999ce3bf08efbcff1acda4ba45d8e1f3b
TrustedCoupon <office@trusted.coupons>
2024-07-09 14:15:25 +0200

    Update middleware.tsx

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,27 +2,27 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const localeRegex = /^[a-z]{2}-[A-Z]{2}$/; // Regular expression pattern to match locales in the format 'xx-XX'
 const defaultLocale = 'en-US'; // Default locale
+const excludeFiles = ['/googlecedb3442f7706abd.html']; // List of files to exclude from locale redirection
 
 export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl;
-  const pathnameHasLocale = localeRegex.test(pathname.split('/')[1]);
 
+  // Check if the pathname should be excluded
+  if (excludeFiles.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  // Check if there is any supported locale in the pathname
+  const pathnameHasLocale = localeRegex.test(pathname.split('/')[1]);
   if (pathnameHasLocale) {
     return;
   }
 
+  // Redirect to the pathname with the default locale
   request.nextUrl.pathname = `/${defaultLocale}${pathname}`;
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
   return NextResponse.redirect(request.nextUrl);
 }
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)'
-    // Optional: only run on root (/) URL
-    // '/'
-  ]
+  matcher: '/((?!_next).*)' // Apply middleware to all paths except for those under _next
 };
