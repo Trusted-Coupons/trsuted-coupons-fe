d26f542d67b3117157119dbac99b3e14fe6123b8
Spawn <office@trusted.coupons>
2024-07-10 14:58:28 +0200

    Update middleware 2

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,34 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const localeRegex = /^[a-z]{2}-[A-Z]{2}$/; // Regular expression pattern to match locales in the format 'xx-XX'
-const defaultLocale = 'en-US'; // Default locale
-const excludeFiles = ['/googlecedb3442f7706abd.html', '/lang.html']; // List of files to exclude from locale redirection
+const localeRegex = /^[a-z]{2}-[A-Z]{2}$/;
+const defaultLocale = 'en-US';
+const excludeFiles = ['/googlecedb3442f7706abd.html', '/lang.html'];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Check if the request is for the root path
   if (pathname === '/') {
-    // Redirect to /lang.html
-    return NextResponse.redirect('/lang.html');
+    return NextResponse.redirect(new URL('/lang.html', request.url));
   }
 
   // Check if the pathname should be excluded from locale redirection
-  if (excludeFiles.includes(pathname)) {
+  if (excludeFiles.some(file => pathname.startsWith(file))) {
     return NextResponse.next();
   }
 
   // Check if there is any supported locale in the pathname
   const pathnameHasLocale = localeRegex.test(pathname.split('/')[1]);
   if (pathnameHasLocale) {
-    return;
+    return NextResponse.next();
   }
 
   // Redirect to the pathname with the default locale
-  request.nextUrl.pathname = `/${defaultLocale}${pathname}`;
-  return NextResponse.redirect(request.nextUrl);
+  return NextResponse.redirect(new URL(`/${defaultLocale}${pathname}`, request.url));
 }
 
 export const config = {
-  matcher: '/((?!_next).*)' // Apply middleware to all paths except for those under _next
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
